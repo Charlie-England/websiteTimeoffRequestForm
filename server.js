d27aa520fd34bcd56ed416fe5ede64aca7c1c433
Charlie England <Charlie265@live.com>
2020-08-12 17:18:24 -0700

    began working on email send feature

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 let app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,6 +12,7 @@ app.get("/",function(req,res) {
 
 app.post("/",function(req, res) {
     req.on("data", function(data) {
+        sendRequestEmail(data);
         console.log(JSON.parse(data));
     })
     res.send("done!")
@@ -21,3 +23,29 @@ app.listen(process.env.PORT || 3000, function() {
     console.log("Server running on port 3000...");
 })
 
+function sendRequestEmail(data) {
+    let parsedData = JSON.parse(data);
+
+    let transporter = nodemailer.createTransport({
+        service: "email", 
+        auth: {
+            user: "facmhwtimeoffrequestfunneler@gmail.com",
+            pass: '766dkfJJu2#'
+        }
+    });
+
+    let mailOptions = {
+        from: "facmhwtimeoffrequestfunneler@gmail.com",
+        to: "facbhsvac@kp.org",
+        subject: "test",
+        text: `${parsedData}`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    })
+}
